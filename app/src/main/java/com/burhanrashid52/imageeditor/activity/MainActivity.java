dc6a1259c1d30890391e2c65621365ff4a54d0ea
housai <hshousai@gmail.com>
2018-10-16 17:37:49 +1100

    pick picture sort picture

--- app/src/main/java/com/burhanrashid52/imageeditor/activity/MainActivity.java
+++ app/src/main/java/com/burhanrashid52/imageeditor/activity/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottom_bar);
         bottomBar.setContainer(R.id.fl_container)
